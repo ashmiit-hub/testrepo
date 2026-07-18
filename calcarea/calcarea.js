87fa9ee5aa8e071afa90fc3dfd66cabfa67495de
ashmit singh <ashmit0829@gmail.com>
2026-07-18 17:11:21 -0400

    web design start

---
+++ calcarea/calcarea.js
@@ -0,0 +1,7 @@
+let length , width;
+function calculateArea(){
+    length=parseFloat(document.getElementById('length').value);
+    width=parseFloat(document.getElementById('width'.value));
+    let area=length*width;
+    document.getElementById('result').innerHTML='the area is: ${area}';
+}
