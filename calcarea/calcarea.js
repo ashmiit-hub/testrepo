cbbf13f192c9f7c74faf6a5d8bd6ddf1be075172
ashmit <ashajshj@gmail.com>
2026-07-18 17:27:13 -0400

    message

--- calcarea/calcarea.js
+++ calcarea/calcarea.js
@@ -3,5 +3,5 @@ function calculateArea(){
     length=parseFloat(document.getElementById('length').value);
     width=parseFloat(document.getElementById('width'.value));
     let area=length*width;
-    document.getElementById('result').innerHTML='the area is: ${area}';
+    document.getElementById('result').innerHTML=`the area is: ${area}`;
 }
